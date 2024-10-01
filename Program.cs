a3a284ea65b0721a97e2b5360ad7a407cf65658b
is-NatashaAguiar <nataaguiar2002@gmail.com>
2024-10-01 07:46:56 -0300

    Implementando os métodos dos celulares

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("IPHONE");
+Smartphone iphone = new Iphone("8569-2147", "Iphone14", "123456", 64);
+iphone.Ligar();
+iphone.InstalarAplicativo("Ifood");
+
+Console.WriteLine("__________________________");
+
+Console.WriteLine("NOKIA");
+Smartphone nokia = new Nokia("8569-1234", "Octa-Core ", "32156", 32);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Burger King");
